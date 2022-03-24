435b0a1d097e93167398f77f9805338589ffe4b6
greenlite <greenlite@greenlites-MacBook-Pro.local>
2022-03-24 12:35:31 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# dro_health_phamarcy
+
+A replica of DRO Health’s pharmacy feature using the Flutter BLOC pattern. 
+
+
---
+++ android/app/src/main/kotlin/com/example/dro_health_phamarcy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dro_health_phamarcy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
